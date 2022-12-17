fee30bf503247f3d194815d25b51f234f211a973
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-12-17 01:07:16 -0300

    implementada flash message de login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render("auth/login");
@@ -5,5 +9,16 @@ module.exports = class AuthController {
 
     static register(req, res) {
         res.render("auth/register");
+    }
+
+    static async registerPost(req, res) {
+        const { name, email, password, confirmPassword } = req.body;
+
+        //password match validation
+        if (password != confirmPassword) {
+            //mensagem de erro
+            req.flash("message", "Senhas diferentes, tente novamente");
+            res.render("auth/register");
+        }
     }
 };
--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,6 +1,8 @@
 const Tought = require("../models/Tought");
 const User = require("../models/User");
 
+const bcrypt = require("bcryptjs");
+
 module.exports = class ToughtController {
     static async showToughts(req, res) {
         res.render("toughts/home");
