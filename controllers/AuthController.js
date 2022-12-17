628c3566afbdf88bc9e87c8bd5429f5b1643032c
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-12-17 01:29:49 -0300

    adicionado registro de usuario + mudança de navbar dependendo da auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -19,6 +19,44 @@ module.exports = class AuthController {
             //mensagem de erro
             req.flash("message", "Senhas diferentes, tente novamente");
             res.render("auth/register");
+            return;
+        }
+
+        //check if user exists
+        const checkIfUserExists = await User.findOne({
+            where: { email: email },
+        });
+
+        if (checkIfUserExists) {
+            //mensagem de erro
+            req.flash("message", "O E-mail já está em uso!");
+            res.render("auth/register");
+            return;
+        }
+
+        //create a password
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword,
+        };
+
+        try {
+            const createdUser = await User.create(user);
+
+            //init session
+            req.session.userid = createdUser.id;
+
+            req.flash("message", "Cadastro criado com sucesso");
+
+            req.session.save(() => {
+                res.redirect("/");
+            });
+        } catch (err) {
+            console.log(err);
         }
     }
 };
